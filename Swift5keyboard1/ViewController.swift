90a3d8274820ec2c7d89fdb74a983e280b7e3049
Yuta <yuta5196@gmail.com>
2020-05-19 19:07:54 +0900

    Initialize repository

--- Swift5keyboard1/ViewController.swift
+++ Swift5keyboard1/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var logoImageVIew: UIImageView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        userNameTextField.delegate = self
+        passWordTextField.delegate = self
+        
     }
 
-
+    @IBAction func login(_ sender: Any) {
+        
+        logoImageVIew.image = UIImage(named: "loginOK")
+        
+        
+        userNameLabel.text = userNameTextField.text
+        passwordLabel.text = passWordTextField.text
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
 }
 
